1eea5d31059f44e23babb1619cf0be7527c178b9
dkarpov <dimon.karpov1@gmail.com>
2022-02-03 09:52:29 +0100

    setup graphql, add chema db integration tbd

---
+++ graphql-server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const schema = require('./schema');
+
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
+
+app.get(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    }),
+);
+
+app.listen(3000, () => {
+    console.log("App listen on port 3000");
+});
+
+// in browser open http://localhost:3000/graphql
+// put query
+
+// {
+//     post(id:1) {
+//         description
+//     }
+// }
+
+// further read :
+// https://www.digitalocean.com/community/tutorials/a-practical-graphql-getting-started-guide-with-nodejs
+// https://www.section.io/engineering-education/build-a-graphql-server-using-nodejs/
---
+++ graphql-server/schema.js
@@ -0,0 +1,95 @@
+const graphql = require('graphql');
+const {GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList} = graphql;
+
+const posts = [
+    {
+        title: 'First post',
+        description: 'Content of the first post',
+        author: 'Flavio'
+    },
+    {
+        title: 'Second post',
+        description: 'Content of the second post',
+        author: 'Roger'
+    }
+];
+
+const authors = {
+    'Flavio': {
+        name: 'Flavio',
+        age: 36
+    },
+    'Roger': {
+        name: 'Roger',
+        age: 7
+    }
+};
+
+const authorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: {
+        name: {
+            type: GraphQLString
+        },
+        age: {
+            type: GraphQLInt
+        }
+    }
+});
+
+const postType = new GraphQLObjectType({
+    name: 'Post',
+    fields: {
+        title: {
+            type: GraphQLString
+        },
+        description: {
+            type: GraphQLString
+        },
+        author: {
+            type: authorType,
+            // Note that a resolver function can be async,
+            // so you can use async/await to lookup resources from a database or the network.
+            resolve: async (source, params) => {
+                return authors[source.author];
+            }
+        }
+    }
+});
+
+const queryType = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        post: {
+            type: postType,
+            args: {
+                id: {type: GraphQLInt}
+            },
+            resolve: (source, {id}) => {
+                return posts[id];
+            }
+        },
+        posts: {
+            type: new GraphQLList(postType),
+            resolve: () => {
+                return posts;
+            }
+        }
+    }
+});
+
+const schema = new GraphQLSchema({
+    query: queryType /*new GraphQLObjectType({
+        name: 'RootQueryType',
+        fields: {
+            hello: {
+                type: GraphQLString,
+                resolve() {
+                    return 'world';
+                }
+            }
+        }
+    })*/
+});
+
+module.exports = schema;
