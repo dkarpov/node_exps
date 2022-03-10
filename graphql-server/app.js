dbdea4e8ebd0b763e858c3edb0662b16dff0326c
dkarpov <dimon.karpov1@gmail.com>
2022-03-10 23:42:04 +0100

    add simple logging with Sentry and locally

--- graphql-server/app.js
+++ graphql-server/app.js
@@ -1,26 +1,69 @@
-const express = require('express');
-const schema = require('./schema');
+const express = require("express");
+const schema = require("./schema");
+const log = require("loglevel");
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
 
-const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
+const graphqlHTTP = require("express-graphql").graphqlHTTP;
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: schema,
-    graphiql: true
-}));
+    graphiql: true,
+  })
+);
 
 app.get(
-    '/graphql',
-    graphqlHTTP({
-        schema,
-        graphiql: true,
-    }),
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
 );
 
 app.listen(3000, () => {
-    console.log("App listen on port 3000");
+  console.log("App listen on port 3000");
+  log.warn("module-tastic");
 });
+
+/////////////// Sentry logging experiments ////////////////
+Sentry.init({
+  dsn: "https://be18d756a74d4c22b5408826734a2320@o1164413.ingest.sentry.io/6253515",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
+
+// to have logs locally wirtten, put below code in conosole
+// $ nodemon app.js > message.log 2> error.log
+
+// Sign up for a free account on https://sentry.io and follow instructions
+// I just copy pasted initial setup code for Sentry
 
 // in browser open http://localhost:3000/graphql
 // put query
